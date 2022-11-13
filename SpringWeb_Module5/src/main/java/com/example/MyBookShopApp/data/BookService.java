73cf61d74bbe9947aa6ebfff1855a0d76e650ba0
SmashFlashDash <salele4715@mail.ru>
2022-11-13 21:08:02 +0300

    init module5

---
+++ SpringWeb_Module5/src/main/java/com/example/MyBookShopApp/data/BookService.java
@@ -0,0 +1,24 @@
+package com.example.MyBookShopApp.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BookService {
+
+    BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getBooksData() {
+        return bookRepository.findAll();
+    }
+}
