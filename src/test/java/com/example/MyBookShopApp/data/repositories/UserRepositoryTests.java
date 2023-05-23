34dfa6042404a2e56ee66f39a90ef6dca078d7df
Alexandr Sodlatov <salele4715@mail.ru>
2023-05-23 15:17:06 +0300

    добавлен тест UserRepository

---
+++ src/test/java/com/example/MyBookShopApp/data/repositories/UserRepositoryTests.java
@@ -0,0 +1,34 @@
+package com.example.MyBookShopApp.data.repositories;
+
+import com.example.MyBookShopApp.data.user.User;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestPropertySource("/application-test.properties")
+class UserRepositoryTests {
+  private final UserRepository userRepository;
+
+  @Autowired
+  public UserRepositoryTests(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Test
+  public void testAddNewUser() {
+    User user = new User();
+    user.setPassword("");
+    user.setBalance(0);
+    user.setName("Name");
+    user.setRegTime(LocalDateTime.now());
+    user.setHash(String.valueOf(user.hashCode()));
+    assertNotNull(userRepository.save(user));
+  }
+}
